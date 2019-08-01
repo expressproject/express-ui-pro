fe5387e1e4014578d45dbda69f40071d79efb8b4
Ashishbarik <ashish.barik1992@gmail.com>
2019-08-01 20:47:17 +0530

    adding signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'login',component:LoginComponent},
+{path:' ',component:WelcomeComponent},
+{path:'signup',component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
